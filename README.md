2b2671a80904692991259bb408cdc8a2c1d31645
Simon Höfle <simon.hoefle@mercedes-benz.com>
2022-12-28 00:21:35 +0100

    ...

---
+++ README.md
@@ -0,0 +1,47 @@
+# aws-pubsub
+
+## Introduction
+Target of this small library is to quickly have a possibility to do 
+realtime-communication between multiple clients and/or servers i.e. for 
+a chat-application.
+The Lib is usable in nodejs applications and in the browser aswell.
+
+## Prerequisites
+To use this library you need a AWSAppSync Realtime API endpoint with an
+valid API-Key.
+In your AWS Account just switch to AppSync service and create such an API:
+
+![](desc1.png)
+
+![](desc2.png)
+
+After creating you need the endpoint URL and you have to create an API-Key.
+You need that information to call the `configure` method of the lib.
+
+![](desc3.png)
+
+## Usage
+```
+pubsub.configure({
+    aws_appsync_graphqlEndpoint:
+        'https://xxxxxxxxxxxxxxxx.appsync-api.eu-central-1.amazonaws.com/graphql',
+    aws_appsync_region: 'eu-central-1',
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+});
+
+pubsub.subscribe(
+    'myChannel',
+    (data) => {
+        console.log('received data', data);
+    },
+    (error) => {
+        // error handling
+    },
+);
+
+pubsub.publish(
+    'myChannel',
+    JSON.stringify({ name: 'A Friend', message: 'Hello World from a friend' }),
+)
+```
---
+++ src/pubsub.ts
@@ -0,0 +1,74 @@
+// @ts-ignore
+import { API, graphqlOperation } from '@aws-amplify/api';
+
+let gotConfigured = false;
+
+const subscribeDoc = /* GraphQL */ `
+    subscription Subscribe($name: String!) {
+        subscribe(name: $name) {
+            data
+            name
+        }
+    }
+`;
+
+const publishDoc = /* GraphQL */ `
+    mutation Publish($data: AWSJSON!, $name: String!) {
+        publish(data: $data, name: $name) {
+            data
+            name
+        }
+    }
+`;
+
+/**
+ * @param  {string} name the name of the channel
+ * @param  {Object} data the data to publish to the channel
+ */
+export async function publish(name: string, data: unknown) {
+    if (!gotConfigured) {
+        throw new Error('Publishing without configuration. Please call "configure" first')
+    }
+
+    return API.graphql(graphqlOperation(publishDoc, {name, data}));
+}
+
+/**
+ * @param  {string} name the name of the channel
+ * @param  {next} next callback function that will be called with subscription payload data
+ * @param  {function} [error] optional function to handle errors
+ * @returns {Observable} an observable subscription object
+ */
+export function subscribe(name: string, next: (data: unknown, provider?: unknown, value?: unknown) => void, error) {
+    if (!gotConfigured) {
+        throw new Error('Subscribing without configuration. Please call "configure" first')
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return API.graphql(graphqlOperation(subscribeDoc, { name })).subscribe({
+        next: ({ provider, value }) => {
+            next(value.data.subscribe, provider, value);
+        },
+        error: error || console.log,
+    });
+}
+
+/**
+ * Has to be called before calling publish or subscribe
+ */
+export function configure(config: {
+    aws_appsync_graphqlEndpoint: string,
+    aws_appsync_region: string,
+    aws_appsync_authenticationType: string,
+    aws_appsync_apiKey: string,
+}) {
+    API.configure(config);
+    gotConfigured = true;
+}
+
+export abstract class PubSub {
+    static publish = publish;
+    static subscribe = subscribe;
+    static configure = configure;
+}
